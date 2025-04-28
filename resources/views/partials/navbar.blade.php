a178e23b933d9d1acfac0c96298614c8f8f97350
dwikurniawati12201279 <dwikurniawati8602@gmail.com>
2025-04-28 19:11:02 +0700

    rubahnama

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -71,7 +71,7 @@
                             <img src="{{ asset('assets/images/avatars/avtar_3.png') }}" alt="User-Profile"
                                 class="theme-color-pink-img img-fluid avatar avatar-50 avatar-rounded" />
                             <div class="caption ms-3 d-none d-md-block">
-                                <h6 class="mb-0 caption-title">Dwi Kurniawan</h6>
+                                <h6 class="mb-0 caption-title">Dwi Kurniawati</h6>
                                 <p class="mb-0 caption-sub-title">Kasir</p>
                             </div>
                         </a>
